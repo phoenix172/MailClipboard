310663875b0c0614964ab9da8a5dcd36c0c2e412
Ivan Yochev <vani172@gmail.com>
2017-12-11 21:23:35 +0200

    first commit

--- MailClipboard/MainWindow.xaml.cs
+++ MailClipboard/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Automation.Text;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +18,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsInput;
+using WindowsInput.Native;
+using NHotkey;
+using NHotkey.Wpf;
 
 namespace MailClipboard
 {
@@ -20,9 +30,55 @@ namespace MailClipboard
     /// </summary>
     public partial class MainWindow : Window
     {
+        NHotkey.Wpf.HotkeyManager _hotkeyManager;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _hotkeyManager = HotkeyManager.Current;
+            _hotkeyManager.AddOrReplace("Email selected text", Key.Q,
+                ModifierKeys.Control | ModifierKeys.Shift , HotkeyPressed);
+        }
+
+        private void HotkeyPressed(object sender, HotkeyEventArgs e)
+        {
+            string selectedText = Clipboard.GetText();
+            SendEmail(selectedText);
+        }
+
+        private static void SendEmail(string selectedText)
+        {
+            var fromAddress = new MailAddress("vani172@gmail.com", "Evernote");
+            var toAddress = new MailAddress("bc13127ea6@nirvanahq.in", "NirvanaHq");
+            const string fromPassword = "rmiuvgnlkarfyyce";
+            string subject = selectedText.Trim();
+            string body = "";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject
+            })
+            { 
+                smtp.Send(message);
+            }
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            _hotkeyManager.Remove("Email selected text");
         }
     }
 }
---
+++ MailClipboard/SelectedTextSource.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MailClipboard
+{
+    static class SelectedTextSource
+    {
+
+        public static string GetText()
+        {
+            const uint WM_GETTEXT = 0x0D;
+
+            const uint WM_GETTEXTLENGTH = 0x0E;
+
+            const uint EM_GETSEL = 0xB0;
+
+            // code needed to get selected text - returns empty string if nothing selected
+
+            IntPtr hWnd = GetForegroundWindow();
+
+            uint processId;
+
+            uint activeThreadId = GetWindowThreadProcessId(hWnd, out processId);
+
+            uint currentThreadId = GetCurrentThreadId();
+
+            AttachThreadInput(activeThreadId, currentThreadId, true);
+
+            IntPtr focusedHandle = GetFocus();
+
+            AttachThreadInput(activeThreadId, currentThreadId, false);
+
+            int len = SendMessage(focusedHandle, WM_GETTEXTLENGTH, 0, null);
+
+            StringBuilder sb = new StringBuilder(len);
+
+            int numChars = SendMessage(focusedHandle, WM_GETTEXT, len + 1, sb);
+
+            int start, next;
+
+            SendMessage(focusedHandle, EM_GETSEL, out start, out next);
+
+            return sb.ToString().Substring(start, next - start);
+        }
+
+        [DllImport("user32.dll")]
+
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll")]
+
+        static extern uint GetCurrentThreadId();
+
+        [DllImport("user32.dll")]
+
+        static extern bool AttachThreadInput(uint idAttach, uint idAttachTo,
+
+            bool fAttach);
+
+        [DllImport("user32.dll")]
+
+        static extern IntPtr GetFocus();
+
+        [DllImport("user32.dll")]
+
+        static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
+
+        // second overload of SendMessage
+
+        [DllImport("user32.dll")]
+
+        static extern int SendMessage(IntPtr hWnd, uint Msg, out int wParam, out int lParam);
+        
+    }
+}
